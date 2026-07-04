4944b11a8960754c6e062ffedfc8e6c6845e43d5
tarrr-byte <ibkdiak7@gmail.com>
2026-07-04 13:55:07 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,30 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "*.supabase.co" },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "mantap77.site",
+      },
+      {
+        protocol: "https",
+        hostname: "www.mantap77.site",
+      },
+      {
+        protocol: "https",
+        hostname: "backend-engine-production-f63f.up.railway.app",
+      },
     ],
   },
+
   allowedDevOrigins: [
     "mantap77.local",
     "mantap77.local:3000",
     "http://mantap77.local:3000",
   ],
 };
-
 
 export default nextConfig;
